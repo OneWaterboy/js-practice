18fab07fbd2e6b5fd8c5a432bf365277d2f8fd8f
OneWaterboy <abrahmrollins@gmail.com>
2021-01-22 11:26:52 -0800

    add in api call

--- weather-app/js/weather-app.js
+++ weather-app/js/weather-app.js
@@ -1,1 +1,10 @@
 "use strict"
+
+const location = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=120&appid={4744f2355efd09f0b9d217be2f0dbec8}";
+
+$.ajax ({
+    url: location, 
+    success: function (result) {
+        console.log(result);
+    }
+})
