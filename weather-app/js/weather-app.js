705bd439376e0c25de7ec3160ef931a7baf3ac68
OneWaterboy <abrahmrollins@gmail.com>
2021-01-22 12:06:29 -0800

    Fixed api URL

--- weather-app/js/weather-app.js
+++ weather-app/js/weather-app.js
@@ -1,9 +1,9 @@
 "use strict"
 
-const location = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=120&appid={4744f2355efd09f0b9d217be2f0dbec8}";
+const url = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=4744f2355efd09f0b9d217be2f0dbec8&units=metric";
 
 $.ajax ({
-    url: location, 
+    url: url, 
     success: function (result) {
         console.log(result);
     }
