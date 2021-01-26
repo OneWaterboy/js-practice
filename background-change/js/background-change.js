f3083c76798aeb1707feea02e882ba3a94b07ba6
OneWaterboy <abrahmrollins@gmail.com>
2021-01-26 09:38:56 -0800

    Finish Background color picker

--- background-change/js/background-change.js
+++ background-change/js/background-change.js
@@ -1,5 +1,14 @@
 "use strict"
 
+let colorValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
 function pickColor() {
-    
+    let hexValue = '#';
+    for (let i=0; i < 6; i++){
+        hexValue += colorValues[Math.floor(Math.random()* colorValues.length)]
+    }
+    console.log(hexValue);
+    document.body.style.backgroundColor = hexValue;
+    document.getElementById('current-color').innerHTML = hexValue;
 }
+
