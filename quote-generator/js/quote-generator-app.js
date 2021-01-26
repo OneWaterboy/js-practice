6c3381f8eedecf48bfbc991942db60e18452249f
OneWaterboy <abrahmrollins@gmail.com>
2021-01-25 16:05:53 -0800

    Setup basic styling
    quote generator

---
+++ quote-generator/js/quote-generator-app.js
@@ -0,0 +1,17 @@
+"use Strict"
+
+let quoteChoice = [
+    'Everyone wants to live on top of the mountain, but all the happiness and growth occurs while you’re climbing it. - Andy Rooney',
+    'Mountains have a way of dealing with overconfidence. - Hermann Buhl',
+    'You can’t move mountains by whispering at them. – Pink',
+    'Mountains are like the great equalizer. It doesn’t matter who anyone is or what they do. – Jimmy Chin',
+    'The hardest mountain to climb is the one within. - J. Lynn',
+    'Because in the end, you won’t remember the time you spent working in the office or mowing your lawn. Climb that goddam mountain. - Jack Kerouac',
+    'Never measure the height of a mountain until you reach the top. Then you will see how low it was. - Dag Hammerskjold'
+]
+
+function pickRandomQuote () {
+    let arrayPosition = Math.floor((Math.random() *7) + 1);
+    console.log
+    document.getElementById('quote-output').innerHTML = <p>`${quoteChoice[arrayPosition]}`</p>;
+}
