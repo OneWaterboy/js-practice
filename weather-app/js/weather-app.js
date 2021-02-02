3fe9a8098daadf7699c27d941d0af33490b7af85
OneWaterboy <abrahmrollins@gmail.com>
2021-02-02 15:59:47 -0800

    fixe javascript
    weather outputs were not display due to trying to make multiple days work. Shifted back to one day so that results show. Working thorugh syntax to display the forecast

--- weather-app/js/weather-app.js
+++ weather-app/js/weather-app.js
@@ -1,18 +1,18 @@
 "use strict"
 
-const url = "http://api.openweathermap.org/data/2.5/forecast?lat=46.6062&lon=-122.3321&appid=4744f2355efd09f0b9d217be2f0dbec8&units=metric";
+const url = "http://api.openweathermap.org/data/2.5/weather?zip=98208&appid=4744f2355efd09f0b9d217be2f0dbec8&units=metric";
 
 $.ajax ({
     url: url, 
     success: function (result) {
         console.log(result);
 
-        $('#location').text(result.list[0].name);
+        $('#location').text(result.name);
         // Return temperature as a string
         let c = Math.round(result.main.temp);
         let degrees = c.toString()
         $('#temperature').text(degrees)
-        $('#sky').text(result.list[0].weather[0].description);
+        $('#sky').text(result.weather[0].description);
         
         // Return wind speed and direction as a string
         let s = Math.round(result.wind.speed)
