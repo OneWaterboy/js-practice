698068ee05855a45a1e8a58ec9c00670f441f709
OneWaterboy <abrahmrollins@gmail.com>
2021-02-04 11:00:50 -0800

    deleted old ideas from code layout

--- game-show-app/js/app.js
+++ game-show-app/js/app.js
@@ -5,12 +5,6 @@ function startGame () {
     document.getElementById('startOverlay').style.display = 'none';
 }
 
-//variables
-
-let wordChoice = Math.floor(Math.random() * (phrases.length + 1))
-let gamePhrase = phrases[wordChoice];
-console.log(gamePhrase);
-
 //phrases array - need to pick a random phrase from this array
 
 const phrases = ["mountaineering", 
@@ -34,30 +28,5 @@ const phrases = ["mountaineering",
                 "snow couloir",
                 "fisherman's bend" ];
 
-//Get Phrase Function
+//Split game phrase into a usable array for game
 
-
-
-function getRandomPhraseAsArray(arr){
-                                           //get random phrase from array [math.random is best option]
-                                            //split array into individual letters
-                                            //return new array containing only characters.
-}
-
-//Add phrase to screen
-
-function addPhraseToDisplay(arr){
-                                            //loop through the array (ie use a for loop on getRandomPhrase
-                                            //For each character in phrases, creat list item
-                                            //place each item into the #phrase ul. If the item is a letter, append the class "letter" to it
-}
-
-
-//Check if the letter is inside of the phrase
-
-function checkLetter(){                     //function needs one parameter equal to the button clicked when player selects a letter
-                                            //function will select everything with a class of "letter"
-                                            //function should loop over all letters and check to see if selected letter is present
-                                            //if match, add class of "show" and then.
-                                            //if no match function should return null
-}
